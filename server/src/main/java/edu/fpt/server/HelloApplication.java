371997ed179d97ca82f54413ddc16c5d34736898
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2023-02-23 09:26:37 +0100

    initial commit

---
+++ server/src/main/java/edu/fpt/server/HelloApplication.java
@@ -0,0 +1,9 @@
+package edu.fpt.server;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
