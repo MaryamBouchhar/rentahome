29a06f4c5dd899a42fc4f56727b82f377d31a388
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2023-02-26 13:01:30 +0100

    import files

--- client/src/admin/components/FormFilePicker.vue
+++ client/src/admin/components/FormFilePicker.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { mdiUpload } from "@mdi/js";
 import { computed, ref, watch } from "vue";
-import BaseButton from "@/components/BaseButton.vue";
+import BaseButton from "../components/BaseButton.vue";
 
 const props = defineProps({
   modelValue: {
--- client/src/admin/components/NotificationBar.vue
+++ client/src/admin/components/NotificationBar.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref, computed, useSlots } from "vue";
 import { mdiClose } from "@mdi/js";
-import { colorsBgLight, colorsOutline } from "@/colors.js";
-import BaseLevel from "@/components/BaseLevel.vue";
-import BaseIcon from "@/components/BaseIcon.vue";
-import BaseButton from "@/components/BaseButton.vue";
+import { colorsBgLight, colorsOutline } from "../js/colors.js";
+import BaseLevel from "../components/BaseLevel.vue";
+import BaseIcon from "../components/BaseIcon.vue";
+import BaseButton from "../components/BaseButton.vue";
 
 const props = defineProps({
   icon: {
