80e9fdbff0e88a260f8d4d032e33b968ff9284f9
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2023-02-26 02:24:22 +0100

    add footer and work with .env with vite

---
+++ client/src/components/client/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <div>
+      <p>Copyright © {{year}} {{app_name}}. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+//current year
+const year = new Date().getFullYear();
+const app_name = import.meta.env.VITE_APP_NAME;
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,16 +2,18 @@
   <div>
     <Hero/>
     <Featured/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Hero from "../components/client/Hero.vue";
 import Featured from "../components/client/Featured.vue";
+import Footer from "../components/client/Footer.vue";
 
 export default {
   name: "Home",
-  components: {Hero, Featured}
+  components: {Hero, Featured, Footer}
 }
 </script>
 
