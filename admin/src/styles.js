41ac494a3bea401981d98137df7fdd1f5ed7312a
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-02-27 09:38:16 +0000

    some changes

--- admin/src/styles.js
+++ admin/src/styles.js
@@ -1,15 +1,4 @@
-export const basic = {
-  aside: "bg-gray-800",
-  asideScrollbars: "aside-scrollbars-gray",
-  asideBrand: "bg-gray-900 text-white",
-  asideMenuItem: "text-gray-300 hover:text-white",
-  asideMenuItemActive: "font-bold text-white",
-  asideMenuDropdown: "bg-gray-700/50",
-  navBarItemLabel: "text-black",
-  navBarItemLabelHover: "hover:text-blue-500",
-  navBarItemLabelActiveColor: "text-blue-600",
-  overlay: "from-gray-700 via-gray-900 to-gray-700",
-};
+
 
 export const white = {
   aside: "bg-white",
