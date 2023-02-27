26917984bd363c108fdb96a9163eb1db0b30f615
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-02-27 14:11:11 +0000

    add new property ui

--- admin/src/main.js
+++ admin/src/main.js
@@ -36,7 +36,7 @@ if (
 }
 
 /* Default title tag */
-const defaultDocumentTitle = "Admin One Vue 3 Tailwind";
+const defaultDocumentTitle = "RentaHome Admin Dashboard";
 
 /* Set document title from route meta */
 router.afterEach((to) => {
