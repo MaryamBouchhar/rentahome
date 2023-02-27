8e5a14f0d4af795c3f1fd104105f819c2f97d4c4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2023-02-27 17:08:13 +0100

    Update router.ts

--- client/src/router/router.ts
+++ client/src/router/router.ts
@@ -1,4 +1,4 @@
-import {RouteRecordRaw, createRouter, createWebHashHistory} from 'vue-router';
+import {RouteRecordRaw, createRouter, createWebHistory} from 'vue-router';
 
 // @ts-ignore
 // @ts-ignore
@@ -36,7 +36,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 });
 
