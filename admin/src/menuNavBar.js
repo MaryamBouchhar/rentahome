97df9744bddae65783bcd01a9ba5f4bd522041f2
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-02-28 10:33:46 +0000

    fix some issues

--- admin/src/menuNavBar.js
+++ admin/src/menuNavBar.js
@@ -13,27 +13,7 @@ import {
 } from "@mdi/js";
 
 export default [
-  {
-    icon: mdiMenu,
-    label: "Sample menu",
-    menu: [
-      {
-        icon: mdiClockOutline,
-        label: "Item One",
-      },
-      {
-        icon: mdiCloud,
-        label: "Item Two",
-      },
-      {
-        isDivider: true,
-      },
-      {
-        icon: mdiCrop,
-        label: "Item Last",
-      },
-    ],
-  },
+
   {
     isCurrentUser: true,
     menu: [
@@ -42,10 +22,7 @@ export default [
         label: "My Profile",
         to: "/profile",
       },
-      {
-        icon: mdiCogOutline,
-        label: "Settings",
-      },
+
       {
         icon: mdiEmail,
         label: "Messages",
