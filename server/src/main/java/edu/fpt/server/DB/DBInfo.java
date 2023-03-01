db05334906107b7e3dc8342e56e8072e453b9c79
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-01 11:25:01 +0100

    Create DBInfo.java

---
+++ server/src/main/java/edu/fpt/server/DB/DBInfo.java
@@ -0,0 +1,8 @@
+package edu.fpt.server.DB;
+
+public class DBInfo {
+    public static final String DB_NAME = "rentahome";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "";
+}
