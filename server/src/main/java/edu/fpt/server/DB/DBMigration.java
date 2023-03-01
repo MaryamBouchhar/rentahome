378a98cb800efecb71d242420bed80b55390b247
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-01 11:12:48 +0100

    Update DBMigration.java

--- server/src/main/java/edu/fpt/server/DB/DBMigration.java
+++ server/src/main/java/edu/fpt/server/DB/DBMigration.java
@@ -1,4 +1,16 @@
 package edu.fpt.server.DB;
 
 public class DBMigration {
+    public static void main(String[] args) {
+        DBMigration migration = new DBMigration();
+        migration.migrate();
+    }
+
+    public void migrate() {
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.connect();
+        dbConnection.createTable();
+        dbConnection.close();
+    }
+}
 }
