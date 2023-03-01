43140adfe6b3e49c6bee6d934ff44a1b2c46620f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-01 22:09:42 +0100

    some changers

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -3,10 +3,15 @@
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Our Story</h1>
+        <h1
+            class="mb-5 text-5xl font-bold"
+            @click="() => {$i18n.locale = 'ar'}"
+        >
+            {{ $t('our_story') }}
+        </h1>
         <div class="flex justify-center space-x-2 mb-5">
-          <div class="badge badge-outline">Most trusted</div>
-          <div class="badge badge-outline">Best prices</div>
+          <div class="badge badge-outline">{{ $t('most_trusted') }}</div>
+          <div class="badge badge-outline">{{ $t('best_prices') }}</div>
           <div class="badge badge-outline">Modern design</div>
         </div>
       </div>
