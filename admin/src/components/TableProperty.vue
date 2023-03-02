509c4b22caa15d492ff2258afb5246b8a50b5ae4
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-03-02 11:12:35 +0000

    some changes

--- admin/src/components/TableProperty.vue
+++ admin/src/components/TableProperty.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { computed, ref } from "vue";
-import { useMainStore } from "@/stores/main";
+import {computed, ref} from "vue";
+import {useMainStore} from "@/stores/main";
 import {mdiEye, mdiImageEdit, mdiTrashCan, mdiUpdate} from "@mdi/js";
 import CardBoxModal from "@/components/CardBoxModal.vue";
 import TableCheckboxCell from "@/components/TableCheckboxCell.vue";
@@ -8,6 +8,8 @@ import BaseLevel from "@/components/BaseLevel.vue";
 import BaseButtons from "@/components/BaseButtons.vue";
 import BaseButton from "@/components/BaseButton.vue";
 import UserAvatar from "@/components/UserAvatar.vue";
+import TableClient from "@/components/TableClient.vue";
+import CardBox from "@/components/CardBox.vue";
 
 defineProps({
   checkable: Boolean,
@@ -73,9 +75,31 @@ const checked = (isChecked, client) => {
 </script>
 
 <template>
-  <CardBoxModal v-model="isModalActive" title="Sample modal">
-    <p>Lorem ipsum dolor sit amet <b>adipiscing elit</b></p>
-    <p>This is sample modal</p>
+
+  <CardBoxModal v-model="isModalActive" title="View Detail Property">
+
+    <span> <b>Category :</b></span>
+    <span class="flex justify-center flex">Villa</span>
+
+    <br>
+    <span> <b>Type :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Bathroom Count :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Room Count :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Area :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Price :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Location :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Publish Date :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Equipped :</b></span><span>Villa</span>
+    <br>
+    <span> <b>Status :</b></span><span>Villa</span>
+    <br>
   </CardBoxModal>
 
   <CardBoxModal
@@ -100,85 +124,85 @@ const checked = (isChecked, client) => {
 
   <table>
     <thead>
-      <tr>
-        <th v-if="checkable" />
-        <th>Id</th>
-        <th>Category</th>
-        <th>Type</th>
-        <th>City</th>
-        <th>Equiped</th>
-        <th>Price</th>
-        <th>status</th>
-        <th>Added at</th>
-        <th />
-      </tr>
+    <tr>
+      <th v-if="checkable"/>
+      <th>Id</th>
+      <th>Category</th>
+      <th>Type</th>
+      <th>City</th>
+      <th>Equiped</th>
+      <th>Price</th>
+      <th>status</th>
+      <th>Added at</th>
+      <th/>
+    </tr>
     </thead>
     <tbody>
-      <tr v-for="client in itemsPaginated" :key="client.id">
-        <TableCheckboxCell
-          v-if="checkable"
-          @checked="checked($event, client)"
-        />
+    <tr v-for="client in itemsPaginated" :key="client.id">
+      <TableCheckboxCell
+        v-if="checkable"
+        @checked="checked($event, client)"
+      />
 
-        <td data-label="Id">
-          {{ client.id }}
-        </td>
+      <td data-label="Id">
+        {{ client.id }}
+      </td>
 
-        <td data-label="Name">
-          {{ client.name }}
-        </td>
-        <td data-label="Company">
-          {{ client.company }}
-        </td>
-        <td data-label="City">
-          {{ client.city }}
-        </td>
-        <td data-label="Progress" class="lg:w-32">
-          <progress
-            class="flex w-2/5 self-center lg:w-full"
-            max="100"
-            :value="client.progress"
-          >
-            {{ client.progress }}
-          </progress>
-        </td>
-        <td data-label="Company">
-          {{ client.company }}
-        </td>
-        <td data-label="City">
-          {{ client.city }}
-        </td>
-        <td data-label="Created" class="lg:w-1 whitespace-nowrap">
-          <small
-            class="text-gray-500 dark:text-slate-400"
-            :title="client.created"
-            >{{ client.created }}</small
-          >
-        </td>
+      <td data-label="Name">
+        {{ client.name }}
+      </td>
+      <td data-label="Company">
+        {{ client.company }}
+      </td>
+      <td data-label="City">
+        {{ client.city }}
+      </td>
+      <td data-label="Progress" class="lg:w-32">
+        <progress
+          class="flex w-2/5 self-center lg:w-full"
+          max="100"
+          :value="client.progress"
+        >
+          {{ client.progress }}
+        </progress>
+      </td>
+      <td data-label="Company">
+        {{ client.company }}
+      </td>
+      <td data-label="City">
+        {{ client.city }}
+      </td>
+      <td data-label="Created" class="lg:w-1 whitespace-nowrap">
+        <small
+          class="text-gray-500 dark:text-slate-400"
+          :title="client.created"
+        >{{ client.created }}</small
+        >
+      </td>
 
-        <td class="before:hidden lg:w-1 whitespace-nowrap">
-          <BaseButtons type="justify-start lg:justify-end" no-wrap>
-            <BaseButton
-              color="info"
-              :icon="mdiEye"
-              small
-              @click="isModalActive = true"
-            />
-            <BaseButton
-              color="danger"
-              :icon="mdiTrashCan"
-              small
-              @click="isModalDangerActive = true"
-            />
-            <BaseButton
-              color="success"
-              :icon="mdiImageEdit"
-              small
-              @click="isModalActive = true"
-            />
-          </BaseButtons>
-        </td>
-      </tr>
+      <td class="before:hidden lg:w-1 whitespace-nowrap">
+        <BaseButtons type="justify-start lg:justify-end" no-wrap>
+          <BaseButton
+            color="info"
+            :icon="mdiEye"
+            small
+            @click="isModalActive = true"
+          />
+          <BaseButton
+            color="danger"
+            :icon="mdiTrashCan"
+            small
+            @click="isModalDangerActive = true"
+          />
+          <BaseButton
+            color="success"
+            :icon="mdiImageEdit"
+            small
+            @click="isModalActive = true"
+          />
+        </BaseButtons>
+      </td>
+    </tr>
     </tbody>
   </table>
   <div class="p-3 lg:px-6 border-t border-gray-100 dark:border-slate-800">
