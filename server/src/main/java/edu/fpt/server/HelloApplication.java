77b41d931e3eefafbe6b4b82437e6365c9b90203
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-02 14:54:04 +0100

    some changes

--- server/src/main/java/edu/fpt/server/HelloApplication.java
+++ server/src/main/java/edu/fpt/server/HelloApplication.java
@@ -1,9 +1,19 @@
 package edu.fpt.server;
 
+import edu.fpt.server.filters.CorsFilter;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
-
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(HelloResource.class);
+        resources.add(CorsFilter.class);
+        return resources;
+    }
 }
--- server/src/main/java/edu/fpt/server/HelloResource.java
+++ server/src/main/java/edu/fpt/server/HelloResource.java
@@ -1,14 +1,30 @@
 package edu.fpt.server;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 
 @Path("/test")
 public class HelloResource {
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces("application/json")
+    public Response hello() {
+        JSONPObject jsonpObject = new JSONPObject("callback", "{\"message\": \"Hello World!\", \"status\": 200}");
+        return Response
+                .ok(jsonpObject)
+                .build();
+    }
+
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response helloPost(String body) {
+        return Response
+                .ok(body)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .build();
     }
 }
