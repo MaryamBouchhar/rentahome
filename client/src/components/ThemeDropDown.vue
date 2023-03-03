6595106f754eeca392c3da4aa074cca461bc8d05
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-04 00:49:48 +0100

    reverse theme dropdown when switch to ar

--- client/src/components/ThemeDropDown.vue
+++ client/src/components/ThemeDropDown.vue
@@ -19,7 +19,7 @@
             class="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl mt-16">
             <div class="grid grid-cols-1 gap-3 p-3" tabindex="0">
                 <div v-for="theme in themes"
-                     class="flex flex-rox-reverse outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2"
+                     class="flex outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2"
                      :class="{'outline': theme === currentTheme}"
                      :title="$t(`themes.${theme}`)"
                      :data-set-theme="theme" data-act-class="outline">
@@ -27,8 +27,9 @@
                          @click="changeTheme(theme)"
                          class="bg-base-100 text-base-content w-full cursor-pointer font-sans">
                         <div class="grid grid-cols-5 grid-rows-3">
-                            <div class="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
-                                <div class="flex-grow text-sm font-bold">{{ $t(`themes.${theme}`) }}</div>
+                            <div :class="{ 'flex-row-reverse': rtl}, { 'flex-row': !rtl}"
+                                class="col-span-5 row-span-3 row-start-1 flex justify-between gap-1 py-3 px-4">
+                                <div class="justify-start text-sm font-bold">{{ $t(`themes.${theme}`) }}</div>
                                 <div class="flex flex-shrink-0 flex-wrap gap-1">
                                     <div class="bg-primary w-2 rounded"></div>
                                     <div class="bg-secondary w-2 rounded"></div>
@@ -118,6 +119,10 @@ export default {
             }
         },
     },
+    mounted() {
+        this.getDefaultTheme();
+        document.documentElement.setAttribute('data-theme', this.currentTheme);
+    }
 }
 </script>
 
