a775f1ab7a9adf5c743b6f6914c06ac0846e9e02
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2023-03-03 10:47:01 +0100

    updates

--- server/src/main/java/edu/fpt/server/Models/Admin.java
+++ server/src/main/java/edu/fpt/server/Models/Admin.java
@@ -1,6 +1,13 @@
 package edu.fpt.server.Models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="admin")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
---
+++ server/src/main/java/edu/fpt/server/ripository/AdminRipository.java
@@ -0,0 +1,7 @@
+package edu.fpt.server.ripository;
+
+import edu.fpt.server.Models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRipository extends JpaRepository<Admin,Integer>{
+}
