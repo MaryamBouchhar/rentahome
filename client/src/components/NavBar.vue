f05802fe81952c25ee98a87d03199b535054913f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-04 21:24:07 +0100

    Update NavBar.vue

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -38,37 +38,7 @@
         <div class="flex basis-1/2 shrink-0 justify-end lg:hidden"></div>
         <div :class="{'flex-row-reverse' : rtl}"
              class="flex flex-row justify-end basis-1/4">
-            <div :class="rtl ? 'dropdown' : 'dropdown dropdown-end'"
-                 class="ml-2">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                        <img src="https://i.pravatar.cc/300"/>
-                    </div>
-                </label>
-                <ul tabindex="0"
-                    :class="rtl ? 'dropdown dropdown-end' : 'dropdown'"
-                    class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                    <li>
-                        <router-link
-                            to="/profile"
-                            class="flex flex-row items-center"
-                            :class="{ 'flex-row-revers' : rtl }"
-                        >
-                            <i class='bx bx-user bx-sm'></i>{{ $t('navbar.profile') }}
-                        </router-link>
-                    </li>
-                    <li>
-                        <a class="flex flex-row items-center">
-                            <i class='flex bx bx-cog bx-sm'></i>{{ $t('navbar.settings') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <i class='flex bx bx-power-off bx-sm'></i>{{ $t('navbar.logout') }}
-                        </a>
-                    </li>
-                </ul>
-            </div>
+
             <ThemeDropDown/>
             <Languages/>
         </div>
