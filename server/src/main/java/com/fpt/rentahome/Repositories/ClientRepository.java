6d941a0979b82f5916fec19db162bf3fdd4b165f
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-03-05 15:47:40 +0000

    some changes

--- server/src/main/java/com/fpt/rentahome/Repositories/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fpt.rentahome.Repositories;
-
-import com.fpt.rentahome.Models.Admin;
-import com.fpt.rentahome.Models.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client,Integer> {
-}
