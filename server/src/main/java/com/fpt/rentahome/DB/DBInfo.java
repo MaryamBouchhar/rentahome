2c9735553c0a090d2b4074d04309a28da34e3edc
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2023-03-05 16:30:20 +0100

    create admin

--- server/src/main/java/com/fpt/rentahome/DB/DBInfo.java
+++ server/src/main/java/com/fpt/rentahome/DB/DBInfo.java
@@ -1,7 +1,7 @@
 package com.fpt.rentahome.DB;
 
 public class DBInfo {
-    public static final String DB_NAME = "login";
+    public static final String DB_NAME = "rentahome";
     public static final String DB_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
     public static final String DB_USER = "root";
     public static final String DB_PASSWORD = "";
