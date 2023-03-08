aba1d841b3357d2666d7d195eb73fabadcf6d961
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-03-08 17:01:20 +0000

    Update NewPropertyView.vue
    Co-Authored-By: Abdellatif Laghjaj <79521157+abdellatif-laghjaj@users.noreply.github.com>

--- admin/src/views/NewPropertyView.vue
+++ admin/src/views/NewPropertyView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {reactive, ref} from "vue";
-import { mdiPlus} from "@mdi/js";
+import {mdiPlus} from "@mdi/js";
 import SectionMain from "@/components/SectionMain.vue";
 import CardBox from "@/components/CardBox.vue";
 import FormCheckRadioGroup from "@/components/FormCheckRadioGroup.vue";
@@ -12,7 +12,6 @@ import BaseButton from "@/components/BaseButton.vue";
 import BaseButtons from "@/components/BaseButtons.vue";
 import LayoutAuthenticated from "@/layouts/LayoutAuthenticated.vue";
 import SectionTitleLineWithButton from "@/components/SectionTitleLineWithButton.vue";
-
 
 
 </script>
@@ -27,7 +26,6 @@ import SectionTitleLineWithButton from "@/components/SectionTitleLineWithButton.
       >
         <router-link to="/properties">
           <BaseButton
-
             target="_blank"
             :icon="mdiProperty"
             label="Show properties"
@@ -40,44 +38,42 @@ import SectionTitleLineWithButton from "@/components/SectionTitleLineWithButton.
       <CardBox form @submit.prevent="submit">
         <FormField>
           <FormField label="Category">
-            <FormControl v-model="form.department" :options="selectOptions"/>
+            <FormControl :options="selectOptions" v-model="property.category"/>
           </FormField>
           <FormField label="Type">
-            <FormControl v-model="form.department" :options="selectOptions"/>
+            <FormControl :options="selectOptions" v-model="property.type"/>
           </FormField>
         </FormField>
 
         <FormField>
-          <FormField label="Bathroom Count ">
-            <FormControl v-model="form.phone" type="number"
-            />
+          <FormField label="Bathroom Count">
+            <FormControl type="number" v-model="property.bathroomCount"/>
           </FormField>
           <FormField label="Room Count ">
-            <FormControl
-              v-model="form.phone"
-              type="number"
-            />
+            <FormControl type="number" v-model="property.roomCount"/>
           </FormField>
         </FormField>
         <FormField>
-
           <FormField label="Area">
-            <FormControl v-model="form.phone" type="text"
-            />
+            <FormControl type="text" v-model="property.area"/>
+          </FormField>
+          <FormField label="Price">
+            <FormControl :options="selectOptions" v-model="property.price"/>
           </FormField>
         </FormField>
         <FormField>
           <FormField label="Location">
-            <FormControl v-model="form.department" :options="selectOptions"/>
+            <FormControl :options="selectOptions" v-model="property.location"/>
           </FormField>
+          <div id="map"></div>
+
           <FormField label="Equiped">
 
             <FormCheckRadioGroup
-              v-model="customElementsForm.switch"
               name="sample-switch"
               type="switch"
-
               :options="{ one: ' '}"
+              v-model="property.equiped"
             />
           </FormField>
         </FormField>
@@ -86,16 +82,17 @@ import SectionTitleLineWithButton from "@/components/SectionTitleLineWithButton.
           <FormControl
             type="textarea"
             placeholder="Put your description here"
+            v-model="property.description"
           />
         </FormField>
-        <FormFilePicker v-model="customElementsForm.file" label="Upload"/>
+        <FormFilePicker label="Upload"/>
         <BaseDivider/>
 
 
         <template #footer>
 
           <BaseButtons class=" flex items-center justify-center">
-            <BaseButton type="submit" color="warning" label="Add"/>
+            <BaseButton type="submit" color="warning" label="Add" @click="addNewProperty"/>
             <BaseButton type="reset" color="warning" outline label="Reset"/>
           </BaseButtons>
 
@@ -106,3 +103,67 @@ import SectionTitleLineWithButton from "@/components/SectionTitleLineWithButton.
 
   </LayoutAuthenticated>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NewPropertyView",
+  data() {
+    return {
+      RESERVATION_API_BASE_URL: "http://localhost:8080/manage-properties",
+      categories: [
+        {value: "one", label: "One"},
+        {value: "two", label: "Two"},
+        {value: "three", label: "Three"},
+      ],
+      types: [
+        {value: "one", label: "One"},
+        {value: "two", label: "Two"},
+        {value: "three", label: "Three"},
+      ],
+      property: {
+        category: "",
+        type: "",
+        bathroomCount: "",
+        roomCount: "",
+        area: "",
+        price: "",
+        location: "",
+        equiped: false,
+        description: "",
+        image: "",
+      },
+    };
+  },
+  methods: {
+    async addNewProperty() {
+      await axios.post(this.RESERVATION_API_BASE_URL + "/add-property", this.property)
+        .then(() => {
+          swal({
+            text: "Property Added Successfully!",
+            icon: "success",
+            closeOnClickOutside: false,
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      console.log(this.property);
+    },
+  },
+  mounted() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: {lat: 37.7749, lng: -122.4194},
+      zoom: 12,
+    });
+  }
+};
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
