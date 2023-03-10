572c8b85d38bb402b3386ae5da5c00693653aa5e
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-10 21:20:10 +0100

    Update ClientAuthController.java

--- server/src/main/java/com/fpt/rentahome/Controllers/Auth/ClientAuthController.java
+++ server/src/main/java/com/fpt/rentahome/Controllers/Auth/ClientAuthController.java
@@ -1,5 +1,42 @@
 package com.fpt.rentahome.Controllers.Auth;
 
-public class ClientAuthController {
+import com.fpt.rentahome.Helpers.ApiResponse;
+import com.fpt.rentahome.Models.Client;
+import com.fpt.rentahome.Services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/client/auth")
+public class ClientAuthController {
+    @Autowired
+    private ClientService clientService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> registerClient(@RequestBody ClientRegistrationRequest clientRegistrationRequest) {
+        // Check if email already exists
+        if (clientService.getClientByEmail(clientRegistrationRequest.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Email address is already taken"));
+        }
+
+        // Create client object from request
+        Client client = Client.builder()
+                .name(clientRegistrationRequest.getName())
+                .email(clientRegistrationRequest.getEmail())
+                .phone(clientRegistrationRequest.getPhone())
+                .password(passwordEncoder.encode(clientRegistrationRequest.getPassword()))
+                .build();
+
+        // Save client to database
+        clientService.saveClient(client);
+
+        return ResponseEntity.ok().body(new ApiResponse(true, "Client registered successfully"));
+    }
 }
