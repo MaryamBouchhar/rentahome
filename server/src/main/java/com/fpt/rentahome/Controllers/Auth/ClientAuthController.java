f765882b5396e5c8c76e9f35681c75f085284ae6
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-10 23:38:15 +0100

    auth backend

--- server/src/main/java/com/fpt/rentahome/Controllers/Auth/ClientAuthController.java
+++ server/src/main/java/com/fpt/rentahome/Controllers/Auth/ClientAuthController.java
@@ -10,13 +10,11 @@ import com.fpt.rentahome.Dto.ClientRegistrationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class ClientAuthController {
