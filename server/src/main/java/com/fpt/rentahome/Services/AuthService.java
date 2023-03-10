a2fdc7f8eac4254d5fce1dac74deb501dacc4066
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-10 21:15:11 +0100

    Create AuthService.java

---
+++ server/src/main/java/com/fpt/rentahome/Services/AuthService.java
@@ -0,0 +1,9 @@
+package com.fpt.rentahome.Services;
+
+import com.fpt.rentahome.Dto.ClientRegistrationRequest;
+import com.fpt.rentahome.Dto.AuthResponse;
+
+public interface AuthService {
+    AuthResponse register(ClientRegistrationRequest clientRegistrationRequest);
+    AuthResponse login(String email, String password);
+}
