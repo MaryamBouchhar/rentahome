2c8065c8ab5b1d1cc74696ed101642d84f3eafbb
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-10 21:20:28 +0100

    Update ClientAuthController.java

--- server/src/main/java/com/fpt/rentahome/Controllers/Auth/ClientAuthController.java
+++ server/src/main/java/com/fpt/rentahome/Controllers/Auth/ClientAuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/client/auth")
+@RequestMapping("/api/auth")
 public class ClientAuthController {
     @Autowired
     private ClientService clientService;
