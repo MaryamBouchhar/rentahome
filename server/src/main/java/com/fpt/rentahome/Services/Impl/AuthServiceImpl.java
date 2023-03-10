5c3d9a9e800632c7e4a19fe157086c959df017fd
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-10 22:07:13 +0100

    Update AuthServiceImpl.java

--- server/src/main/java/com/fpt/rentahome/Services/Impl/AuthServiceImpl.java
+++ server/src/main/java/com/fpt/rentahome/Services/Impl/AuthServiceImpl.java
@@ -1,11 +1,11 @@
 package com.fpt.rentahome.services.Impl;
 
-import com.fpt.rentahome.Dto.ClientRequestDto;
+import com.fpt.rentahome.Dto.ClientRegistrationRequest;
 import com.fpt.rentahome.Helpers.AuthResponse;
 import com.fpt.rentahome.Helpers.ApiResponse;
+import com.fpt.rentahome.Helpers.JWT.JwtTokenProvider;
 import com.fpt.rentahome.Models.Client;
 import com.fpt.rentahome.Repositories.ClientRepository;
-import com.fpt.rentahome.Security.jwt.JwtTokenProvider;
 import com.fpt.rentahome.Services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -51,7 +51,7 @@ public class AuthServiceImpl implements AuthService {
 
         clientRepository.save(client);
 
-        String accessToken = jwtTokenProvider.createToken(client.getEmail(), client.getRole().toString());
+        String accessToken = jwtTokenProvider.generateToken(client.getEmail());
 
         return new AuthResponse(true, "Registration Successful", accessToken);
     }
@@ -61,7 +61,7 @@ public class AuthServiceImpl implements AuthService {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-            String accessToken = jwtTokenProvider.createToken(email, userDetails.getAuthorities().toString());
+            String accessToken = jwtTokenProvider.generateToken(email);
             return new AuthResponse(true, "Login Successful", accessToken);
         } catch (AuthenticationException e) {
             return new AuthResponse(false, "Invalid email/password supplied", null);
