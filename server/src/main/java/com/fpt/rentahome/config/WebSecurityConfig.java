a486948ac45f3b53720c578594feacd5bb347d32
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-03-10 23:21:29 +0100

    Update WebSecurityConfig.java

--- server/src/main/java/com/fpt/rentahome/config/WebSecurityConfig.java
+++ server/src/main/java/com/fpt/rentahome/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("*").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
