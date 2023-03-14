b2292623089dca7400681815344a04d9587ee86d
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2023-03-14 23:32:21 +0100

    Update PropertyCard.vue

--- client/src/components/PropertyCard.vue
+++ client/src/components/PropertyCard.vue
@@ -3,7 +3,7 @@
       class="card w-full bg-base-100 shadow-xl">
     <figure class="relative h-52">
       <router-link :key="property.id" :to="'/properties/' + property.id">
-        <img src='https://picsum.photos/500/300?random=5' :alt="property.category" class="card-image object-cover w-full h-full"/>
+        <img :src="property.image" :alt="property.category" class="card-image object-cover w-full h-full"/>
       </router-link>
     </figure>
     <div class="card-body">
@@ -12,7 +12,7 @@
           <router-link :key="property.id" :to="'/properties/' + property.id">
             {{ property.category }} in {{ property.location.city }}
           </router-link>
-          <div v-if="property.status == 'Available'" class="badge badge-success">{{ property.status }}</div>
+          <div v-if="property.status === 'Available'" class="badge badge-success">{{ property.status }}</div>
           <div v-else class="badge badge-error">{{ property.status }}</div>
         </h2>
       </div>
