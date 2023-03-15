473d71124d78f0e484d1d18eceeae21906e03dda
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2023-03-15 15:34:28 +0100

    Update WishlistController.java

--- server/src/main/java/com/fpt/rentahome/Controllers/WishlistController.java
+++ server/src/main/java/com/fpt/rentahome/Controllers/WishlistController.java
@@ -1,6 +1,7 @@
 package com.fpt.rentahome.Controllers;
 
 
+import com.fpt.rentahome.Dto.AddToWishlistRequest;
 import com.fpt.rentahome.Helpers.ApiResponse;
 import com.fpt.rentahome.Models.Client;
 import com.fpt.rentahome.Models.Property;
@@ -15,21 +16,40 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin
 @RestController
-@RequestMapping("/wishlist")
+@RequestMapping("/api/wishlist")
 public class WishlistController {
 
     @Autowired
     WishlistService wishlistService;
 
-    @PostMapping("/add/{propertyId}")
-    public Wishlist addPropertyToWishlist(@PathVariable Integer propertyId, Authentication authentication) {
-        Client client = (Client) authentication.getPrincipal();
 
-        return wishlistService.addPropertyToWishlist(propertyId,client);
-
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse> addPropertyToWishlist(@RequestBody AddToWishlistRequest request) {
+        wishlistService.addPropertyToWishlist(request.getPropertyId(), request.getUserId());
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Add to wishlist"), HttpStatus.CREATED);
     }
+
+
+    @GetMapping("/{client_id}")
+    public ResponseEntity<List<Property>> getWishList(@PathVariable("client_id") Integer clientId) {
+
+        List<Wishlist> body = wishlistService.readWishList(clientId);
+        List<Property> properties = new ArrayList<Property>();
+        for (Wishlist wishList : body) {
+         properties.add(wishList.getProperty());
+        }
+
+        return new ResponseEntity<List<Property>>(properties, HttpStatus.OK);
+    }
+
+
+
+
 
 
 
