1e6a91b8ddbce40c99421a36459dadf2a74c9006
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2023-03-15 01:07:51 +0100

    Create WishlistRepository.java

---
+++ server/src/main/java/com/fpt/rentahome/Repositories/WishlistRepository.java
@@ -0,0 +1,16 @@
+package com.fpt.rentahome.Repositories;
+
+import com.fpt.rentahome.Models.Client;
+import com.fpt.rentahome.Models.Property;
+import com.fpt.rentahome.Models.Wishlist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<Wishlist,Integer> {
+
+    List<Wishlist> findByClient(Client client);
+}
