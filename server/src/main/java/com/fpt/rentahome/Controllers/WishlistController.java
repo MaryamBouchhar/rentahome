048168f03abf0aa33a9a6e94794b434bbf162d58
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2023-03-15 11:11:35 +0100

    Update WishlistController.java

--- server/src/main/java/com/fpt/rentahome/Controllers/WishlistController.java
+++ server/src/main/java/com/fpt/rentahome/Controllers/WishlistController.java
@@ -2,11 +2,17 @@ package com.fpt.rentahome.Controllers;
 
 
 import com.fpt.rentahome.Helpers.ApiResponse;
+import com.fpt.rentahome.Models.Client;
+import com.fpt.rentahome.Models.Property;
 import com.fpt.rentahome.Models.Wishlist;
+import com.fpt.rentahome.Repositories.PropertyRepository;
 import com.fpt.rentahome.Services.WishlistService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -15,15 +21,17 @@ import org.springframework.web.bind.annotation.*;
 public class WishlistController {
 
     @Autowired
-    private WishlistService wishlistService;
+    WishlistService wishlistService;
 
-    @PostMapping("/add")
-    public ResponseEntity<ApiResponse> addItemToWishlist(@RequestBody Wishlist wishlistItem){
+    @PostMapping("/add/{propertyId}")
+    public Wishlist addPropertyToWishlist(@PathVariable Integer propertyId, Authentication authentication) {
+        Client client = (Client) authentication.getPrincipal();
 
-        wishlistService.addItemToWishlist(wishlistItem);
+        return wishlistService.addPropertyToWishlist(propertyId,client);
 
-        return new ResponseEntity<>(new ApiResponse(true, "ITEM ADDED TO WISHLIST"), HttpStatus.CREATED);
     }
+
+
 
 }
 
